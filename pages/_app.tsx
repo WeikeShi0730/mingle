97fb653eead669fed10941fbaf6781d8ea2cd1e1
Weike Shi <weikeshih@gmail.com>
2022-09-20 23:52:08 -0400

    Clean up api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,2 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
